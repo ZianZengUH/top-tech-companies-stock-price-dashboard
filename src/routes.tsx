e6da2020836a21b35a2213abe74fef6af8e9f290
Michael Chun <mhkfchun@hawaii.edu>
2023-11-18 21:40:27 -1000

    Added New Financial Charts Page
    Adds Financial Charts page with modified layout with 4 charts and drop down menus for each that display each available company's ticker.

--- src/routes.tsx
+++ src/routes.tsx
@@ -63,6 +63,12 @@ const routes: IRoute[] = [
     path: '/rtl-default',
     icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
   },
+  {
+    name: 'Financial Charts',
+    layout: '/admin',
+    path: '/financial',
+    icon: <Icon as={MdBarChart} width="20px" height="20px" color="inherit" />,
+  },
 ];
 
 export default routes;
