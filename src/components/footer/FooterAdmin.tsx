09ef045c1a92108f315572eb87833cc2ecda94ea
FeiyiChen01 <77761439+FeiyiChen01@users.noreply.github.com>
2023-11-21 15:03:54 -1000

    logo

--- src/components/footer/FooterAdmin.tsx
+++ src/components/footer/FooterAdmin.tsx
@@ -2,30 +2,30 @@
 
 import {
   Flex,
-  Link,
-  List,
-  ListItem,
   Text,
   useColorModeValue,
+  Image,
+  HStack
 } from '@chakra-ui/react';
+import Teamlogo from 'img/team-logo/team-logo.jpg';
+
 
 export default function Footer() {
   const textColor = useColorModeValue('gray.400', 'white');
   return (
-    <Flex
+<HStack
       zIndex="3"
-      flexDirection={{
-        base: 'column',
-        xl: 'row',
-      }}
-      alignItems={{
-        base: 'center',
-        xl: 'start',
-      }}
-      justifyContent="space-between"
+      justifyContent="center"
+      alignItems="center"
       px={{ base: '30px', md: '50px' }}
       pb="30px"
     >
+                            <Image
+  borderRadius="full"
+  boxSize="60px"
+  src={Teamlogo.src}
+  alt="team-logo"
+/>
       <Text
         color={textColor}
         textAlign={{
@@ -35,73 +35,11 @@ export default function Footer() {
         mb={{ base: '20px', xl: '0px' }}
       >
         {' '}
-        &copy; {new Date().getFullYear()}
         <Text as="span" fontWeight="500" ms="4px">
-          Horizon UI. All Rights Reserved. Made with love by
-          <Link
-            mx="3px"
-            color={textColor}
-            href="https://www.simmmple.com"
-            target="_blank"
-            fontWeight="700"
-          >
-            Simmmple!
-          </Link>
+        Created by Zephyrus‘ Flame Mapper
+        &copy; {new Date().getFullYear()}
         </Text>
       </Text>
-      <List display="flex">
-        <ListItem
-          me={{
-            base: '20px',
-            md: '44px',
-          }}
-        >
-          <Link
-            fontWeight="500"
-            color={textColor}
-            href="mailto:hello@simmmple.com"
-          >
-            Support
-          </Link>
-        </ListItem>
-        <ListItem
-          me={{
-            base: '20px',
-            md: '44px',
-          }}
-        >
-          <Link
-            fontWeight="500"
-            color={textColor}
-            href="https://www.simmmple.com/licenses"
-          >
-            License
-          </Link>
-        </ListItem>
-        <ListItem
-          me={{
-            base: '20px',
-            md: '44px',
-          }}
-        >
-          <Link
-            fontWeight="500"
-            color={textColor}
-            href="https://simmmple.com/terms-of-service"
-          >
-            Terms of Use
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link
-            fontWeight="500"
-            color={textColor}
-            href="https://www.blog.simmmple.com/"
-          >
-            Blog
-          </Link>
-        </ListItem>
-      </List>
-    </Flex>
+      </HStack>
   );
 }
--- src/components/sidebar/components/Brand.tsx
+++ src/components/sidebar/components/Brand.tsx
@@ -1,19 +1,31 @@
 // Chakra imports
-import { Flex, useColorModeValue } from '@chakra-ui/react';
+import { Flex, useColorModeValue, Text, Image } from '@chakra-ui/react';
 
 // Custom components
 import { HorizonLogo } from 'components/icons/Icons';
 import { HSeparator } from 'components/separator/Separator';
+import ProjectLogo from 'img/projectlogo/projectlogo.png'
+import Teamlogo from 'img/team-logo/team-logo.jpg';
+import Project from 'views/admin/profile/components/Project';
 
 export function SidebarBrand() {
 	//   Chakra color mode
 	let logoColor = useColorModeValue('navy.700', 'white');
 
 	return (
-		<Flex alignItems='center' flexDirection='column'>
-			<HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
-			<p> Zephyrus‘ Flame Mapper </p>
-			<HSeparator mb='20px' />
+		<Flex alignItems='center' flexDirection='row'>
+			<Image
+  borderRadius="full"
+  boxSize="100px"
+  src={Teamlogo.src}
+  alt="ProjectLogo"
+/>
+
+			<Image
+  boxSize="100px"
+  src={ProjectLogo.src}
+  alt="ProjectLogo"
+/>
 		</Flex>
 	);
 }
